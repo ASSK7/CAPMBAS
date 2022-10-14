757718af845299737dfa298672da1630762bd94e
Afreed Shaik <afreedshaik143@gmail.com>
2022-10-14 10:28:00 +0000

    added Custom CRUD Operation in srv/mysampleservice

--- srv/mysampleservice.cds
+++ srv/mysampleservice.cds
@@ -1,5 +1,20 @@
+using { afreed.db.master, afreed.db.transaction } from '../db/datamodel';
+
+
 service mysrv {
 
-     function myFunction(msg: String) returns String;    
+     function myFunction(msg: String) returns String;   // this was created in DAY 1 of CAPM Class 
+
+     @readonly
+     entity ReadEmployeeSrv as projection on master.employees; //here master.employees represents output data structure
+     @insertonly
+     entity InsertEmployeeSrv as projection on master.employees;
+     @updateonly
+     entity UpdateEmployeeSrv as projection on master.employees;
+     @deleteonly
+     entity DeleteEmployeeSrv as projection  on master.employees;
+
+     //we can do all CRUD operatons on single entity but for practice I am doing this
+
 
 }
--- srv/mysampleservice.js
+++ srv/mysampleservice.js
@@ -1,7 +1,104 @@
-const mysrvdemo = function(srv){
+const cds = require('@sap/cds');
+const { employees } = cds.entities("afreed.db.master");
+
+
+const mysrvdemo = function(srv){   // this was created in DAY 1 of CAPM Class
      srv.on("myFunction",(req,res) => {
          return "Hello " + req.data.msg;
      });
+
+
+
+//Generic Handlers
+//READ
+srv.on('READ','ReadEmployeeSrv', async(req,res) => {
+    var results = [];
+
+    //cds query language - SELECT * FROM
+    // results = await cds.tx(req).run(SELECT.from(employees));
+    // results = await cds.tx(req).run(SELECT.from(employees).limit(3)); //limiting top 3  records
+    //tx means transaction which means select query
+
+    //cds query to read single record with where condition
+    // results = await cds.tx(req).run(SELECT.from(employees).where({"nameFirst" : "Susan"}));
+
+    //cds query to record if user passes key like /Enitty/key
+    var whereCondition = req.data;
+    if(whereCondition.hasOwnProperty("ID")){ //if it is ID field
+        results = await cds.tx(req).run(SELECT.from(employees).where(whereCondition));
+    } 
+    else{
+        results = await cds.tx(req).run(SELECT.from(employees).limit(1));
+    }
+
+
+    return results;
+});
+
+
+//CREATE
+srv.on('CREATE','InsertEmployeeSrv', async(req,res) => {
+    //For DML(INSERT,UPDATE,DELETE) statement we use .transaction instead of tx
+    let returnData = await cds.transaction(req).run([
+        INSERT.into(employees).entries([req.data])
+    ]).then( (resolve,reject) => {
+        if(typeof(resolve) !== undefined){
+            return  req.data;
+        }
+        else{
+            req.error(500,"There was an issue in insert");
+        }
+    }).catch(err => {
+       req.error(500, "There was error "+err.toString());
+    });
+    return returnData ;
+
+});
+
+
+//UPDATE
+srv.on('UPDATE','UpdateEmployeeSrv',async(req,res) => {
+    returnData = await cds.transaction(req).run([
+        UPDATE(employees).set({
+            nameFirst : req.data.nameFirst
+        }).where({ID : req.data.ID}) ,
+
+        UPDATE(employees).set({
+            nameLast : req.data.nameLast
+        }).where({ID : req.data.ID})
+
+    ]).then( (resolve,reject) => {
+        if(typeof(resolve) !== undefined){
+            return  req.data;
+        }
+        else{
+            req.error(500,"There was an issue in Update");
+        }
+    }).catch(err => {
+       req.error(500, "There was error "+err.toString());
+    });
+    return returnData ;
+});
+
+
+//Delete
+srv.on('DELETE','DeleteEmployeeSrv',async(req,res) => {
+    returnData = await cds.transaction(req).run([
+        DELETE.from(employees).where(req.data)
+    ]).then( (resolve,reject) => {
+        if(typeof(resolve) !== undefined){
+            return  req.data;
+        }
+        else{
+            req.error(500,"There was an issue in Update");
+        }
+    }).catch(err => {
+       req.error(500, "There was error "+err.toString());
+    });
+    return returnData ;
+});
+
+
 }
 
 module.exports = mysrvdemo; 
